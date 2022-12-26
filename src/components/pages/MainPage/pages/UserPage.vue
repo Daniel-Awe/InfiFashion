5e79546449b6eebd28e4eaa48c0441a49ca7eb91
曾海洛 <a1282737390@126.com>
2022-12-26 20:51:32 +0800

    我的页面图标替换

--- src/components/pages/MainPage/pages/UserPage.vue
+++ src/components/pages/MainPage/pages/UserPage.vue
@@ -1,52 +1,64 @@
 <template>
-  <div class="container column" style="position: relative">
-    <TopBar class="topBar" style="position: relative">
-      <div
-        class="row"
-        style="margin: 0.9375rem 1.25rem; justify-content: space-between"
-      >
-        <div class="switchButton row">
-          <img :src="require('@/assets/svg/switch_line.svg')" />
-          <span>切换为人才</span>
+    <div class="container column" style="position: relative">
+        <TopBar class="topBar" style="position: relative">
+            <div
+                class="row"
+                style="
+                    margin: 0.9375rem 1.25rem;
+                    justify-content: space-between;
+                "
+            >
+                <div class="switchButton row">
+                    <img :src="require('@/assets/svg/switch_line.svg')" />
+                    <span>切换为人才</span>
+                </div>
+                <img
+                    :src="require('@/assets/svg/setting_line.svg')"
+                    @click="setting"
+                />
+            </div>
+        </TopBar>
+        <div class="view column" style="position: absolute; top: 6.75rem">
+            <!-- 用户信息框 -->
+            <div
+                v-loading="!user"
+                class="userInfoBox column"
+                style="
+                    padding: 2.5rem 0.625rem 0.625rem 0.625rem;
+                    position: relative;
+                "
+            >
+                <img
+                    style="position: absolute; top: -1.75rem"
+                    class="avatar"
+                    :src="tryGet(user, 'avatar')"
+                    alt=""
+                />
+                <div class="primary">{{ tryGet(user, "name") }}</div>
+                <TagBox
+                    style="margin-top: 0.3125rem"
+                    :tags="tryGet(user, 'tags')"
+                    :editable="true"
+                />
+                <div style="margin-top: 0.625rem" class="row">
+                    <span class="secondary">综合评分：</span>
+                    <ScoreBox :score="tryGet(user, 'score')" />
+                </div>
+            </div>
+            <OperationBar
+                style="width: 100%; margin-top: 15px"
+                :datas="tryGet(datas, 'rowButtons')"
+            />
+            <div style="width: 100%; margin-top: 15px">
+                <OperationItem
+                    v-for="(item, key) in tryGet(datas, 'options')"
+                    :key="key"
+                    :data="item"
+                    @click="$router.push({ name: item.routeName })"
+                />
+            </div>
         </div>
-        <img :src="require('@/assets/svg/setting_line.svg')" @click="setting" />
-      </div>
-    </TopBar>
-    <div class="view column" style="position: absolute; top: 6.75rem">
-      <!-- 用户信息框 -->
-      <div
-        v-loading="!user"
-        class="userInfoBox column"
-        style="padding: 2.5rem 0.625rem 0.625rem 0.625rem; position: relative"
-      >
-        <img
-          style="position: absolute; top: -1.75rem"
-          class="avatar"
-          :src="tryGet(user, 'avatar')"
-          alt=""
-        />
-        <div class="primary">{{ tryGet(user, "name") }}</div>
-        <TagBox
-          style="margin-top: 0.3125rem"
-          :tags="tryGet(user, 'tags')"
-          :editable="true"
-        />
-        <div style="margin-top: 0.625rem" class="row">
-          <span class="secondary">综合评分：</span>
-          <ScoreBox :score="tryGet(user, 'score')" />
-        </div>
-      </div>
-      <OperationBar style="width: 100%; margin-top: 15px" :datas="rowButtons" />
-      <div style="width: 100%; margin-top: 15px">
-        <OperationItem
-          v-for="(item, key) in options"
-          :key="key"
-          :data="item"
-          @click="$router.push({ name: item.routeName })"
-        />
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -58,124 +70,184 @@ import ScoreBox from "./UserPage/ScoreBox.vue";
 import OperationBar from "./UserPage/OperationBar.vue";
 import OperationItem from "./UserPage/OperationItem.vue";
 export default {
-  name: "UserPage",
-  components: { TopBar, TagBox, ScoreBox, OperationBar, OperationItem },
-  data() {
-    return {
-      rowButtons: [
-        {
-          title: "历史记录",
-          iconUrl: require("@/assets/svg/user/histroy.svg"),
-          routeName: undefined,
-        },
-        {
-          title: "我的订单",
-          iconUrl: require("@/assets/svg/user/trade_list.svg"),
-          routeName: undefined,
-        },
-        {
-          title: "我的关注",
-          iconUrl: require("@/assets/svg/user/attention.svg"),
-          routeName: undefined,
-        },
-      ],
-      options: [
-        {
-          title: "我的需求",
-          iconUrl: require("@/assets/svg/user/protocol.svg"),
-          routeName: undefined,
-        },
-        {
-          title: "我的收藏",
-          iconUrl: require("@/assets/svg/user/collect.svg"),
-          routeName: "CollectionPage",
-        },
-        {
-          title: "我的客服",
-          iconUrl: require("@/assets/svg/user/headset.svg"),
-          routeName: undefined,
-        },
-        {
-          title: "意见与反馈",
-          iconUrl: require("@/assets/svg/user/notepad.svg"),
-          routeName: undefined,
-        },
-      ],
-    };
-  },
-  methods: {
-    setting() {
-      this.$router.push({
-        name: "SettingPage",
-      });
+    name: "UserPage",
+    components: { TopBar, TagBox, ScoreBox, OperationBar, OperationItem },
+    data() {
+        return {
+            merchant: {
+                rowButtons: [
+                    {
+                        title: "历史记录",
+                        iconUrl: require("@/assets/svg/user/merchant_histroy.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "我的订单",
+                        iconUrl: require("@/assets/svg/user/merchant_trade_list.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "我的关注",
+                        iconUrl: require("@/assets/svg/user/merchant_attention.svg"),
+                        routeName: undefined,
+                    },
+                ],
+                options: [
+                    {
+                        title: "我的需求",
+                        iconUrl: require("@/assets/svg/user/merchant_protocol.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "我的收藏",
+                        iconUrl: require("@/assets/svg/user/merchant_collect.svg"),
+                        routeName: "CollectionPage",
+                    },
+                    {
+                        title: "我的客服",
+                        iconUrl: require("@/assets/svg/user/merchant_headset.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "意见与反馈",
+                        iconUrl: require("@/assets/svg/user/merchant_notepad.svg"),
+                        routeName: undefined,
+                    },
+                ],
+            },
+            talent: {
+                rowButtons: [
+                    {
+                        title: "历史记录",
+                        iconUrl: require("@/assets/svg/user/talent_histroy.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "我的发布",
+                        iconUrl: require("@/assets/svg/user/talent_post.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "我的关注",
+                        iconUrl: require("@/assets/svg/user/talent_attention.svg"),
+                        routeName: undefined,
+                    },
+                ],
+                options: [
+                    {
+                        title: "我的收藏",
+                        iconUrl: require("@/assets/svg/user/talent_collect.svg"),
+                        routeName: "CollectionPage",
+                    },
+                    {
+                        title: "订单详情",
+                        iconUrl: require("@/assets/svg/user/talent_trade_list.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "团队介绍",
+                        iconUrl: require("@/assets/svg/user/talent_group.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "我的客服",
+                        iconUrl: require("@/assets/svg/user/talent_headset.svg"),
+                        routeName: undefined,
+                    },
+                    {
+                        title: "意见与反馈",
+                        iconUrl: require("@/assets/svg/user/talent_notepad.svg"),
+                        routeName: undefined,
+                    },
+                ],
+            },
+        };
     },
-  },
-  computed: {
-    ...mapGetters("loginInfo", ["isLogin"]),
-    ...mapState("loginInfo", ["user"]),
-  },
-  mixins: [helper],
+    methods: {
+        setting() {
+            this.$router.push({
+                name: "SettingPage",
+            });
+        },
+    },
+    computed: {
+        datas() {
+            switch (this.type) {
+                case "merchant":
+                    return this.merchant;
+
+                case "talent":
+                    return this.talent;
+
+                default:
+                    return null;
+            }
+        },
+        ...mapGetters("loginInfo", ["isLogin", "type"]),
+        ...mapState("loginInfo", ["user"]),
+    },
+    mixins: [helper],
 };
 </script>
 
 <style lang="scss" scoped>
 .container {
-  width: 100%;
-  height: 100%;
-
-  overflow-y: scroll;
-
-  .topBar {
-    height: 11.25rem;
-
-    .switchButton {
-      padding: 0.0625rem 0.6875rem 0.0625rem 0.3125rem;
-      width: max-content;
-
-      border-radius: 1.25rem;
-      background: rgba(249, 249, 249, 0.6);
-
-      @include font-size-style(4);
-      font-weight: 500;
-      color: $--color-black;
-    }
-  }
-
-  .view {
     width: 100%;
+    height: 100%;
 
-    .userInfoBox {
-      box-sizing: border-box;
-      width: 20rem;
-      border-radius: 0.3125rem;
-      background-color: $--color-white;
+    overflow-y: scroll;
 
-      .avatar {
-        width: 3.625rem;
-        height: 3.625rem;
-        border-radius: 50%;
-        object-fit: cover;
-      }
+    .topBar {
+        height: 11.25rem;
 
-      .primary {
-        @include font-size-style(3);
-        font-weight: 700;
-        color: $--color-black;
-      }
+        .switchButton {
+            padding: 0.0625rem 0.6875rem 0.0625rem 0.3125rem;
+            width: max-content;
 
-      .secondary {
-        @include font-size-style(4);
-        font-weight: 500;
-        color: $--color-black;
-      }
+            border-radius: 1.25rem;
+            background: rgba(249, 249, 249, 0.6);
+
+            @include font-size-style(4);
+            font-weight: 500;
+            color: $--color-black;
+        }
     }
-  }
+
+    .view {
+        width: 100%;
+
+        .userInfoBox {
+            box-sizing: border-box;
+            width: 20rem;
+            border-radius: 0.3125rem;
+            background-color: $--color-white;
+
+            .avatar {
+                width: 3.625rem;
+                height: 3.625rem;
+                border-radius: 50%;
+                object-fit: cover;
+            }
+
+            .primary {
+                @include font-size-style(3);
+                font-weight: 700;
+                color: $--color-black;
+            }
+
+            .secondary {
+                @include font-size-style(4);
+                font-weight: 500;
+                color: $--color-black;
+            }
+        }
+    }
 }
 
 .row {
-  @include flex-style(row);
+    @include flex-style(row);
 }
 .column {
-  @include flex-style(column);
+    @include flex-style(column);
 }
 </style>
--- src/components/pages/MainPage/pages/UserPage/ScoreBox.vue
+++ src/components/pages/MainPage/pages/UserPage/ScoreBox.vue
@@ -4,16 +4,32 @@
             v-for="(item, key) in [1, 2, 3, 4, 5]"
             :class="score >= item ? '' : 'lighter'"
             :key="key"
-            :src="require('@/assets/svg/star.svg')"
+            :src="starUrl"
         />
     </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
     name: "ScoreBox",
     props: {
         score: Number,
+    },
+    computed: {
+        starUrl() {
+            switch (this.type) {
+                case "merchant":
+                    return require("@/assets/svg/merchant_star.svg");
+
+                case "talent":
+                    return require("@/assets/svg/talent_star.svg");
+
+                default:
+                    return null;
+            }
+        },
+        ...mapGetters("loginInfo", ["type"]),
     },
 };
 </script>
