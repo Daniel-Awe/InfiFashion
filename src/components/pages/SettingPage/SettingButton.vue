84de9c5e56256cc37205987b4338924dd0d550b6
Daniel-Awe <1270355676@qq.com>
2023-03-31 11:48:37 +0800

    优化了SettingButton

--- src/components/pages/SettingPage/SettingButton.vue
+++ src/components/pages/SettingPage/SettingButton.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <div class="button" @click="$router.push({ name: points.routeName })" style="margin-top: 0" :class="points.detail ? 'detailnoexist' : 'detailexist'">
+    <div class="container" @click="$router.push({ name: points.routeName })" style="margin-top: 0" :class="points.detail ? 'detailyes' : 'detailno'">
       <div>
-        <div class="title">{{ points.title }}</div>
+        <div class="setting">{{ points.setting }}</div>
         <div class="detail" v-if="points.detail">{{ points.detail }}</div>
       </div>
       <div class="right">
-        <img :src="require('@/assets/svg/arrow_right.svg')" />
+        <!-- 软件版本 -->
+        <div v-if="points.prompt" class="prompt">{{ points.prompt }}</div>
+        <!-- 新消息通知 -->
+        <div v-else-if="points.notifyswitch" class="notify">
+          {{ points.notifyswitch ? '已开启' : '已关闭' }}
+        </div>
+        <!-- 右箭头 -->
+        <img :src="require('@/assets/svg/arrow_right.svg')" v-else />
       </div>
     </div>
     <div class="gap" v-if="points.gap == true"></div>
@@ -29,16 +36,17 @@ export default {
   background-color: $--background-color-base;
 }
 
-.button {
+.container {
   position: relative;
   width: 100%;
   background-color: #fff;
   border-bottom: 1px solid $--border-color-base;
   box-sizing: border-box;
-  .title {
+  .setting {
     height: 20px;
     font-size: 14px;
     text-align: left;
+    font-weight: 500;
   }
   .detail {
     height: 17px;
@@ -51,12 +59,19 @@ export default {
     right: 16px;
     top: 50%;
     transform: translateY(-50%);
+    .prompt {
+      color: rgba(128, 128, 128, 1);
+    }
+    .notify {
+      color: rgba(128, 128, 128, 1);
+      font-size: 14px;
+    }
   }
 }
-.detailnoexist {
+.detailyes {
   padding: 9px 16px 7.5px 20px;
 }
-.detailexist {
+.detailno {
   padding: 12px 16px 12px 20px;
 }
 </style>
