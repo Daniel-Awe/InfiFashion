90e1952a563eea0536cd8e81c31ca0708096b0c8
Daniel-Awe <1270355676@qq.com>
2023-03-31 11:03:57 +0800

    设置页面的按钮

---
+++ src/components/pages/SettingPage/SettingSwitch.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="Switch" style="margin-top: 0">
+      <div class="setting">{{ points.setting }}</div>
+      <div class="right" @click="inputValue">
+        <el-switch v-model="Value"></el-switch>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SettingSwitch',
+  props: {
+    points: Object
+  },
+  data() {
+    return {
+      Value: this.points.value
+    }
+  },
+  methods: {
+    inputValue() {
+      this.$emit('update-value', this.points.id, this.Value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Switch {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: #fff;
+  border-bottom: 1px solid $--border-color-base;
+  box-sizing: border-box;
+  padding: 12px 16px 12px 20px;
+  .setting {
+    height: 20px;
+    font-size: 14px;
+    text-align: left;
+    font-weight: 500;
+  }
+}
+</style>
