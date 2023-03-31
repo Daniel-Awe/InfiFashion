bc31038600f2815e2f7a2e182d8b5492e947ba68
Daniel-Awe <1270355676@qq.com>
2023-03-31 11:24:03 +0800

    隐私管理页面

--- src/components/pages/SettingPage/pages/PrivacyManagementPage.vue
+++ src/components/pages/SettingPage/pages/PrivacyManagementPage.vue
@@ -6,7 +6,7 @@
       <SettingButton v-for="(item, id) in permissions" :key="id" :points="item" @click="$router.push({ name: item.routeName })"></SettingButton>
     </div>
     <div class="box">
-      <div class="topic">权限管理</div>
+      <div class="topic gap">隐私说明</div>
       <SettingButton v-for="(item, id) in privacy" :key="id" :points="item" @click="$router.push({ name: item.routeName })"></SettingButton>
     </div>
   </div>
@@ -22,28 +22,23 @@ export default {
     return {
       permissions: [
         {
-          id: '1',
-          title: '推荐管理',
+          setting: '推荐管理',
           detail: '管理个性化推荐内容展示',
           routeName: ''
         },
         {
-          id: '2',
-          title: '营销类电话管理',
+          setting: '营销类电话管理',
           detail: '开启后，将为你推荐更符合你需求的优质服务',
-          routeName: '',
-          gap: true
+          routeName: ''
         }
       ],
       privacy: [
         {
-          id: '1',
-          title: '个人信息保护政策',
+          setting: '个人信息保护政策',
           routeName: ''
         },
         {
-          id: '2',
-          title: '个人信息下载',
+          setting: '个人信息下载',
           routeName: ''
         }
       ]
@@ -59,11 +54,14 @@ export default {
   background-color: $--background-color-base;
 }
 .topic {
-  height: 17px;
+  height: 22px;
   width: 100%;
   font-size: 12px;
   color: rgba(51, 51, 51, 1);
   text-align: left;
   padding: 0 0 2px 20px;
+}
+.gap {
+  padding-top: 10px;
 }
 </style>
