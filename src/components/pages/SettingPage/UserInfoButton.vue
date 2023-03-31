593e7fbe6f048085bfbd9a71e5a129baae23749d
Daniel-Awe <1270355676@qq.com>
2023-03-31 11:27:46 +0800

    个人信息栏组件

---
+++ src/components/pages/SettingPage/UserInfoButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="info">
+      <p class="left"><slot name="setting"></slot></p>
+      <slot name="descripte"></slot>
+      <div class="right">
+        <img :src="require('@/assets/svg/arrow_right.svg')" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfoButton',
+  props: ['setting', 'descripte']
+}
+</script>
+
+<style lang="scss" scoped>
+.info {
+  display: flex;
+  position: relative;
+  background-color: #fff;
+  font-size: 14px;
+  padding-left: 20px;
+  border-bottom: 1px solid $--border-color-base;
+  p {
+    height: 20px;
+  }
+  .left {
+    width: 85px;
+    font-weight: 500;
+  }
+  .introduce {
+    width: 250px;
+    color: $--color-info;
+    overflow: hidden;
+  }
+  .right {
+    position: absolute;
+    right: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
