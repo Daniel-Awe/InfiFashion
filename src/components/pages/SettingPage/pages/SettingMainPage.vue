c67a128be08ede86defe2bc06bb59efbec816d25
Daniel-Awe <1270355676@qq.com>
2023-03-31 11:24:46 +0800

    添加了‘关于infifashion’的链接

--- src/components/pages/SettingPage/pages/SettingMainPage.vue
+++ src/components/pages/SettingPage/pages/SettingMainPage.vue
@@ -4,7 +4,7 @@
       <template #default>设置</template>
     </SettingTopBar>
     <div class="main">
-      <SettingButton v-for="(item, id) in setting" :key="id" :points="item" @click="$router.push({ name: item.routeName })"></SettingButton>
+      <SettingButton v-for="(item, key) in setting" :key="key" :points="item" @click="$router.push({ name: item.routeName })"></SettingButton>
     </div>
     <CrossButton title="切换账号" class="button" />
     <CrossButton title="退出登录" class="button" @click="logout" />
@@ -27,60 +27,51 @@ export default {
     return {
       setting: [
         {
-          id: '1',
-          title: '个人信息',
+          setting: '个人信息',
           detail: '头像、昵称、收获地址',
           routeName: 'UserInfoPage'
         },
         {
-          id: '2',
-          title: '账号与安全',
+          setting: '账号与安全',
           detail: '修改密码、修改手机号、实名认证',
           routeName: 'CountSecurityPage',
           gap: true
         },
         {
-          id: '3',
-          title: '消息通知设置',
+          setting: '消息通知设置',
           detail: '',
-          routeName: ''
+          routeName: 'MessageNotifyPage'
         },
         {
-          id: '4',
-          title: '隐私管理',
+          setting: '隐私管理',
           detail: '',
           routeName: 'PrivacyManagementPage'
         },
         {
-          id: '5',
-          title: '身份切换',
+          setting: '身份切换',
           detail: '',
           routeName: 'IdentitySwitchPage',
           gap: true
         },
         {
-          id: '6',
-          title: '个人信息收集清单',
+          setting: '个人信息收集清单',
           detail: '',
           routeName: ''
         },
         {
-          id: '7',
-          title: '第三方共享个人信息清单',
+          setting: '第三方共享个人信息清单',
           detail: '',
           routeName: ''
         },
         {
-          id: '8',
-          title: '了解我们',
+          setting: '了解我们',
           detail: '',
           routeName: ''
         },
         {
-          id: '9',
-          title: '关于infiFashion',
+          setting: '关于infiFashion',
           detail: '',
-          routeName: ''
+          routeName: 'AboutInfiFashionPage'
         }
       ]
     }
