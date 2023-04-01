40cfd8426a275dc47492739cef7b67db50b6dd34
Daniel-Awe <1270355676@qq.com>
2023-04-01 22:37:51 +0800

    关注页面完善

--- src/assets/demo/datas.js
+++ src/assets/demo/datas.js
@@ -1,25 +1,27 @@
-import users from "./datas/users"
-import articles from "./datas/articles"
-import dialogues from "./datas/dialogues"
-import services from "./datas/services"
-import collections from "./datas/collections"
-import requirements from "./datas/requirements"
-import history from "./datas/history"
-import orders from "./datas/orders"
+import users from './datas/users'
+import articles from './datas/articles'
+import dialogues from './datas/dialogues'
+import services from './datas/services'
+import collections from './datas/collections'
+import requirements from './datas/requirements'
+import history from './datas/history'
+import orders from './datas/orders'
 
-import home from "./datas/home"
-import forum from "./datas/forum"
+import home from './datas/home'
+import forum from './datas/forum'
+import following from './datas/following'
 
 export default {
-    users,
-    articles,
-    dialogues,
-    services,
-    collections,
-    requirements,
-    history,
-    orders,
+  users,
+  articles,
+  dialogues,
+  services,
+  collections,
+  requirements,
+  history,
+  orders,
 
-    home,
-    forum
-};
+  home,
+  forum,
+  following
+}
